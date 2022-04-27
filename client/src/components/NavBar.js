2e27b985a6039dc3dcedb8c49a6025d2dbb05527
SvenAventador <82192661+SvenAventador@users.noreply.github.com>
2022-04-27 19:38:27 +0300

    feat: Завершил работу над страницей определенного устройства, разобрался с хуками. Следующее: Админ панель

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,12 +5,14 @@ import Nav from "react-bootstrap/Nav"
 import Container from "react-bootstrap/Container"
 import { NavLink } from 'react-router-dom'
 import { Button } from 'react-bootstrap'
-import { SHOP_ROUTE } from '../utils/consts'
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts'
 import { observer } from "mobx-react-lite"
+import { useNavigate } from 'react-router-dom'
 
 const NavBar = observer( () => {
 
   const { user } = useContext(Context)
+  const history = useNavigate()
 
   return (
 
@@ -20,8 +22,8 @@ const NavBar = observer( () => {
         {user.isAuth ?
 
           <Nav className="ml-auto" style={{ color: 'white' }}>
-            <Button variant={"outline-light"}>Админ панель</Button>
-            <Button variant={"outline-light"} className='ml-2'>Войти</Button>
+            <Button variant={"outline-light"} onClick={() => history(ADMIN_ROUTE)}>Админ панель</Button>
+            <Button variant={"outline-light"} onClick={() => history(LOGIN_ROUTE)} className='ml-2'>Выйти</Button>
           </Nav>
 
           :
